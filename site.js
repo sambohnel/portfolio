52833a34bf40617422744bef2ea9ec68324f29d2
sambohnel <sambohnel@yahoo.co.uk>
2017-05-08 13:34:29 +0000

    first commit

---
+++ site.js
@@ -0,0 +1,6 @@
+
+// Any time an element with the class of "tagline" is clicked
+$(".tagline").click(function() {
+  	// Change elements of the h1 tag so their inner text says "jQuery works..."
+    $("h1").text("jQuery works...");
+});
